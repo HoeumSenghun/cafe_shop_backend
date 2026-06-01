c40078ee0d16c73236e71fb0fed6e2468cf05f70
HoeumSenghun <hoeumsenghun369@gmail.com>
2026-06-01 10:04:51 +0700

    20260601-init project

---
+++ src/main/java/com/cafe_shop/CafeShopApplication.java
@@ -0,0 +1,13 @@
+package com.cafe_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cafe_shop/CafeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
